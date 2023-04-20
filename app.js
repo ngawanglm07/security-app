a1e423e13cd3a7f2669f47a2bfba6b510f228409
Your Name <you@example.com>
2023-04-20 13:45:25 +0530

    SWD

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
-require(dotenv).config()
+
 
 
 require('mongodb');
